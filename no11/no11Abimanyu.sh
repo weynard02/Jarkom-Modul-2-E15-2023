c717f7a59f39ede9bdcc4c55737f55d31e4ce94d
weynard02 <samsicoweynard@gmail.com>
2023-10-12 00:57:34 +0700

    no11 progres

---
+++ no11/no11Abimanyu.sh
@@ -0,0 +1,22 @@
+apt-get install apache2 -y
+service apache2 start
+
+echo `<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostn$        # the server uses to identify itself. This is used when c$        # redirection URLs. In the context of virtual hosts, the $        # specifies what hostname must appear in the request's Ho$        # match this virtual host. For the default virtual host ($        # value is not decisive as it is used as a last resort ho$        # However, you must set it for any further virtual host e$        #ServerName www.example.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/abimanyu.E15
+        ServerName abimanyu.E15.com
+        ServerAlias www.abimanyu.E15.com
+
+        # Available loglevels: trace8, ..., trace1, debug, info, $        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for parti$        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, whic$        # enabled or disabled at a global level, it is possible to        # include a line for only one particular virtual host. Fo$        # following line enables the CGI configuration for this h$        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+        </VirtualHost>
+    ` > /etc/apache2/sites-available/000-default.conf
