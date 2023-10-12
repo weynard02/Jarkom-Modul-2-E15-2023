6dae9487bbb8805f9a3f64fe391aa6775f988f96
weynard02 <samsicoweynard@gmail.com>
2023-10-12 19:57:34 +0700

    update 20

--- no20/no20Abimanyu.sh
+++ no20/no20Abimanyu.sh
@@ -1,14 +1,14 @@
 # Karena website www.parikesit.abimanyu.yyy.com semakin banyak pengunjung dan banyak gambar gambar random, maka ubahlah request gambar yang memiliki substring “abimanyu” akan diarahkan menuju abimanyu.png.
 
 echo 'RewriteEngine On
-RewriteCond %{REQUEST_URI} ^/public/images/(.*)(abimanyu)(.*\.(png|jpg))
+RewriteCond %{REQUEST_URI} ^/public/images/(.*)(abimanyu)(.*\.(png|jpg|jpeg|webp|bmp|tiff|svg))
 RewriteCond %{REQUEST_URI} !/public/images/abimanyu.png
 RewriteRule abimanyu http://parikesit.abimanyu.E15.com/public/images/abimanyu.png$1 [L,R=301]' > /var/www/parikesit.abimanyu.E15/.htaccess
 
 echo -e '<VirtualHost *:80>
+
   ServerAdmin webmaster@localhost
   DocumentRoot /var/www/parikesit.abimanyu.E15
-
   ServerName parikesit.abimanyu.E15.com
   ServerAlias www.parikesit.abimanyu.E15.com
 
@@ -25,8 +25,10 @@ echo -e '<VirtualHost *:80>
           AllowOverride All
   </Directory>
 
-  Alias "/public" "/var/www/parikesit.abimanyu.E15/public"
-  Alias "/secret" "/var/www/parikesit.abimanyu.E15/secret"
+  <Directory /var/www/parikesit.abimanyu.E15/public/js>
+        Options +Indexes
+  </Directory>
+
   Alias "/js" "/var/www/parikesit.abimanyu.E15/public/js"
 
   ErrorDocument 404 /error/404.html
