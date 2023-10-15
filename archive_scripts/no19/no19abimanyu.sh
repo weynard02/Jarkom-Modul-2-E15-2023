10f5501bc59a47d1ff3c11eeefcb8ac4b335fc31
Alexander Weynard Samsico <90879937+weynard02@users.noreply.github.com>
2023-10-15 10:45:47 +0700

    Update no19abimanyu.sh

--- archive_scripts/no19/no19abimanyu.sh
+++ archive_scripts/no19/no19abimanyu.sh
@@ -4,12 +4,10 @@ echo '<VirtualHost *:80>
         # The ServerName directive sets the request scheme, hostn$        # the server uses to identi$
 
         ServerAdmin webmaster@localhost
-        DocumentRoot /var/www/parikesit.abimanyu.E15
+        DocumentRoot /var/www/abimanyu.E15
         ServerName 10.44.3.2
         RedirectPermanent / http://www.abimanyu.E15.com
-
-        # Available loglevels: trace8, ..., trace1, debug, info, $        # error, crit, alert, emerg.        # It is also possible to configure the loglevel for parti$        # modules, $        #LogLevel info ssl:warn
-
+        
         ErrorLog ${APACHE_LOG_DIR}/error.log
         CustomLog ${APACHE_LOG_DIR}/access.log combined
 
